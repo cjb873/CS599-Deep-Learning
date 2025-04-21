59f56be957a60661b91b8f008cdccc9dc7cae471
cjb873 <cjb873@nau.edu>
2025-04-20 23:23:40 -0700

    Final

--- Assignment4/CNN_assignment4.py
+++ Assignment4/CNN_assignment4.py
@@ -1,210 +1,245 @@
-# -*- coding: utf-8 -*-
-"""CNN_week9.ipynb
-
-IST597 :- Implementing CNN from scratch
-Week 9 Tutorial
-
-Author:- aam35
-"""
-
-import tensorflow as tf
+import torch.nn as nn
+import torch
+import torchvision
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torch.optim as optim
+from torch.nn.utils.parametrizations import weight_norm as WN
+import pandas as pd
 import numpy as np
-import time
-import tensorflow.contrib.eager as tfe
-tf.enable_eager_execution()
-tf.executing_eagerly()
-seed = 1234
-tf.random.set_random_seed(seed=seed)
-np.random.seed(seed)
+import matplotlib.pyplot as plt
 
-from tensorflow.examples.tutorials.mnist import input_data
-data = input_data.read_data_sets("/tmp/data/", one_hot=True, reshape=False)
 
-batch_size = 64
-hidden_size = 100
-learning_rate = 0.01
-output_size = 10
+transform = transforms.Compose(
+    [transforms.ToTensor()])
 
-class CNN(object):
-  def __init__(self,hidden_size,output_size,device=None):
-      filter_h, filter_w, filter_c , filter_n = 5 ,5 ,1 ,30
-      self.W1 = tf.Variable(tf.random_normal([filter_h, filter_w, filter_c, filter_n], stddev=0.1))
-      self.b1 = tf.Variable(tf.zeros([filter_n]),dtype=tf.float32)
-      self.W2 = tf.Variable(tf.random_normal([14*14*filter_n, hidden_size], stddev=0.1))
-      self.b2 = tf.Variable(tf.zeros([hidden_size]),dtype=tf.float32)
-      self.W3 = tf.Variable(tf.random_normal([hidden_size, output_size], stddev=0.1))
-      self.b3 = tf.Variable(tf.zeros([output_size]),dtype=tf.float32)
-      self.variables = [self.W1,self.b1, self.W2, self.b2, self.W3, self.b3]
-      self.device = device
-      self.size_output = output_size
-  
-  def flatten(self,X, window_h, window_w, window_c, out_h, out_w, stride=1, padding=0):
-    
-      X_padded = tf.pad(X, [[0,0], [padding, padding], [padding, padding], [0,0]])
+batch_size = 250
 
-      windows = []
-      for y in range(out_h):
-          for x in range(out_w):
-              window = tf.slice(X_padded, [0, y*stride, x*stride, 0], [-1, window_h, window_w, -1])
-              windows.append(window)
-      stacked = tf.stack(windows) # shape : [out_h, out_w, n, filter_h, filter_w, c]
+trainset = torchvision.datasets.FashionMNIST(root='./data', train=True,
+                                             download=True,
+                                             transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
 
-      return tf.reshape(stacked, [-1, window_c*window_w*window_h])
-  
-  def convolution(self,X, W, b, padding, stride):
-      n, h, w, c = map(lambda d: d.value, X.get_shape())
-      #print(X.get_shape())
-      #print(data.train.images.get_shape())
-      filter_h, filter_w, filter_c, filter_n = [d.value for d in W.get_shape()]
-    
-      out_h = (h + 2*padding - filter_h)//stride + 1
-      out_w = (w + 2*padding - filter_w)//stride + 1
+testset = torchvision.datasets.FashionMNIST(root='./data', train=False,
+                                            download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
 
-      X_flat = self.flatten(X, filter_h, filter_w, filter_c, out_h, out_w, stride, padding)
-      W_flat = tf.reshape(W, [filter_h*filter_w*filter_c, filter_n])
-    
-      z = tf.matmul(X_flat, W_flat) + b     # b: 1 X filter_n
-    
-      return tf.transpose(tf.reshape(z, [out_h, out_w, n, filter_n]), [2, 0, 1, 3])
-    
- 
-    
-  def relu(self,X):
-      return tf.maximum(X, tf.zeros_like(X))
-    
-  def max_pool(self,X, pool_h, pool_w, padding, stride):
-      n, h, w, c = [d.value for d in X.get_shape()]
-    
-      out_h = (h + 2*padding - pool_h)//stride + 1
-      out_w = (w + 2*padding - pool_w)//stride + 1
+classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+           'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
 
-      X_flat = self.flatten(X, pool_h, pool_w, c, out_h, out_w, stride, padding)
 
-      pool = tf.reduce_max(tf.reshape(X_flat, [out_h, out_w, n, pool_h*pool_w, c]), axis=3)
-      return tf.transpose(pool, [2, 0, 1, 3])
+class CNN(nn.Module):
+    def __init__(self, hidden_size, output_size, optim, norm):
+        super().__init__()
+        filter_h, filter_w, filter_c, filter_n = 5, 5, 1, 30
 
-    
-  def affine(self,X, W, b):
-      n = X.get_shape()[0].value # number of samples
-      X_flat = tf.reshape(X, [n, -1])
-      return tf.matmul(X_flat, W) + b 
-    
-  def softmax(self,X):
-      X_centered = X - tf.reduce_max(X) # to avoid overflow
-      X_exp = tf.exp(X_centered)
-      exp_sum = tf.reduce_sum(X_exp, axis=1)
-      return tf.transpose(tf.transpose(X_exp) / exp_sum) 
-    
-  
-  def cross_entropy_error(self,yhat, y):
-      return -tf.reduce_mean(tf.log(tf.reduce_sum(yhat * y, axis=1)))
-    
-  
-  def forward(self,X):
-      if self.device is not None:
-        with tf.device('gpu:0' if self.device == 'gpu' else 'cpu'):
-          self.y = self.compute_output(X)
-      else:
-        self.y = self.compute_output(X)
-      
-      return self.y
-    
-    
-  def loss(self, y_pred, y_true):
-      '''
-      y_pred - Tensor of shape (batch_size, size_output)
-      y_true - Tensor of shape (batch_size, size_output)
-      '''
-      y_true_tf = tf.cast(tf.reshape(y_true, (-1, self.size_output)), dtype=tf.float32)
-      y_pred_tf = tf.cast(y_pred, dtype=tf.float32)
-      return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=y_pred_tf, labels=y_true_tf))
-    
-    
-  def backward(self, X_train, y_train):
-      """
-      backward pass
-      """
-      # optimizer
-      # Test with SGD,Adam, RMSProp
-      optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-      #predicted = self.forward(X_train)
-      #current_loss = self.loss(predicted, y_train)
-      #optimizer.minimize(current_loss, self.variables)
+        self.conv1 = nn.Conv2d(filter_c, filter_n, (filter_w, filter_h))
 
-      #optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-      with tf.GradientTape() as tape:
-          predicted = self.forward(X_train)
-          current_loss = self.loss(predicted, y_train)
-      #print(predicted)
-      #print(current_loss)
-      #current_loss_tf = tf.cast(current_loss, dtype=tf.float32)
-      grads = tape.gradient(current_loss, self.variables)
-      optimizer.apply_gradients(zip(grads, self.variables),
-                              global_step=tf.train.get_or_create_global_step())
-      
-      
-  def compute_output(self,X):
-      conv_layer1 = self.convolution(X, self.W1, self.b1, padding=2, stride=1)
-      conv_activation = self.relu(conv_layer1)
-      conv_pool = self.max_pool(conv_activation, pool_h=2, pool_w=2, padding=0, stride=2)
-      conv_affine =self.affine(conv_pool, self.W2,self.b2)
-      conv_affine_activation = self.relu(conv_affine)
-      
-      conv_affine_1 = self.affine(conv_affine_activation, self.W3, self.b3)
-      return conv_affine_1
+        self.norm1 = None
+        self.norm2 = None
 
-def accuracy_function(yhat,true_y):
-  correct_prediction = tf.equal(tf.argmax(yhat, 1), tf.argmax(true_y, 1))
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  return accuracy
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(12 * 12 * filter_n, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
-# Initialize model using GPU
-mlp_on_cpu = CNN(hidden_size,output_size, device='gpu')
+        if norm is LayerNorm or norm is nn.LayerNorm:
+            self.norm1 = norm([filter_n, 24, 24])
+            self.norm2 = norm([hidden_size])
+        elif norm is BatchNorm or norm is nn.BatchNorm2d:
+            self.norm1 = norm(filter_n)
+            self.norm2 = norm(hidden_size, 2)
+        elif norm is WeightNorm or norm is WN:
+            self.fc1 = norm(self.fc1)
+            # self.conv1 = norm(self.conv1)
+        if norm is nn.BatchNorm2d:
+            self.norm2 = nn.BatchNorm1d(hidden_size)
 
-num_epochs = 4
-train_x =  tf.convert_to_tensor(data.train.images)
-train_y = tf.convert_to_tensor(data.train.labels)
-time_start = time.time()
-num_train = 55000
-z= 0
+        self.optim = optim(self.parameters(), lr=0.001)
+        self.loss_fn = nn.CrossEntropyLoss()
 
-for epoch in range(num_epochs):
-        train_ds = tf.data.Dataset.from_tensor_slices((data.train.images, data.train.labels)).map(lambda x, y: (x, tf.cast(y, tf.float32)))\
-           .shuffle(buffer_size=1000)\
-           .batch(batch_size=batch_size)
-        loss_total = tf.Variable(0, dtype=tf.float32)
-        accuracy_total = tf.Variable(0, dtype=tf.float32)
-        for inputs, outputs in train_ds:
-            preds = mlp_on_cpu.forward(inputs)
-            loss_total = loss_total + mlp_on_cpu.loss(preds, outputs)
-#             accuracy_train = accuracy_function(preds,outputs)
-#             accuracy_total = accuracy_total + accuracy_train
-            mlp_on_cpu.backward(inputs, outputs)
-            #print(z)
-            #z = z+ 1
-        print('Number of Epoch = {} - loss:= {:.4f}'.format(epoch + 1, loss_total.numpy() / num_train))
-        preds = mlp_on_cpu.compute_output(train_x)
-        accuracy_train = accuracy_function(preds,train_y)
-        
-        accuracy_train = accuracy_train * 100
-        print ("Training Accuracy = {}".format(accuracy_train.numpy()))
-        
-        
-#         preds_val = mlp_on_cpu.compute_output(data.validation.images)
-#         accuracy_val = accuracy_function(preds_val,data.validation.labels)
-#         accuracy_val = accuracy_val * 100
-#         print ("Validation Accuracy = {}".format(accuracy_val.numpy()))
- 
-#test accuracy
-test_x =  tf.convert_to_tensor(data.test.images)
-test_y = tf.convert_to_tensor(data.test.labels)
-preds_test = mlp_on_cpu.compute_output(test_x)
-accuracy_test = accuracy_function(preds_test,test_y)
-# To keep sizes compatible with model
-accuracy_test = accuracy_test * 100
-print ("Test Accuracy = {}".format(accuracy_test.numpy()))
+    def forward(self, x):
+        x = self.conv1(x)
+        if self.norm1 is not None:
+            x = self.norm1(x)
+        x = self.pool(x)
+        x = F.relu(x)
+        x = x.view(-1, 12 * 12 * 30)
+        x = self.fc1(x)
+        if self.norm2 is not None:
+            x = self.norm2(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
 
-        
-# time_taken = time.time() - time_start
-# print('\nTotal time taken (in seconds): {:.2f}'.format(time_taken))
-# #For per epoch_time = Total_Time / Number_of_epochs
+    def train_step(self, loader):
+        self.train()
+
+        for data in loader:
+            inputs, labels = data
+
+            self.optim.zero_grad()
+            outputs = self.forward(inputs)
+            loss = self.loss_fn(outputs, labels)
+            loss.backward()
+            self.optim.step()
+
+        return loss.item()
+
+    def test_model(self, loader):
+        self.eval()
+        total = 0
+        correct = 0
+        for data in loader:
+            images, labels = data
+
+            outputs = self.forward(images)
+
+            _, predicted = torch.max(outputs, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        return correct / total
+
+
+class BatchNorm(nn.Module):
+
+    def __init__(self, n=30, n_dim=4, epsilon=1e-5):
+        super().__init__()
+
+        self.n_dim = n_dim
+        shape = (1, n, 1, 1)
+        if self.n_dim == 2:
+            shape = (1, n)
+        self.epsilon = epsilon
+        self.mu = None
+        self.var = None
+        self.gamma = torch.nn.Parameter(torch.ones(shape))
+        self.beta = torch.nn.Parameter(torch.zeros(shape))
+
+    def forward(self, x):
+        if self.training:
+            if self.n_dim == 2:
+                self.mu = x.mean(axis=0)
+                self.var = x.var(axis=0, unbiased=True)
+            else:
+                self.mu = x.mean((0, 2, 3), keepdim=True)
+                self.var = x.var((0, 2, 3), keepdim=True, unbiased=True)
+
+        x_hat = (x - self.mu) \
+            / torch.sqrt(self.var + self.epsilon)
+        return x_hat * self.gamma + \
+            self.beta
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, normalized_shape=[30, 24, 24],  epsilon=1e-5):
+        super().__init__()
+        self.epsilon = epsilon
+        self.mu = None
+        self.var = None
+        self.gamma = torch.nn.Parameter(torch.ones(normalized_shape))
+        self.beta = torch.nn.Parameter(torch.zeros(normalized_shape))
+        self.dim = [-(dim + 1) for dim, _ in enumerate(normalized_shape)]
+
+    def forward(self, x):
+        if self.training:
+            self.mu = x.mean(self.dim, keepdim=True)
+            self.var = x.var(self.dim, keepdim=True)
+
+        x_hat = (x - self.mu) \
+            / torch.sqrt(self.var + self.epsilon)
+        return x_hat * self.gamma + \
+            self.beta
+
+
+class WeightNorm(nn.Module):
+    def __init__(self, layer: torch.nn.Module, name='weight'):
+        super().__init__()
+        self.layer = layer
+
+        self.v = torch.nn.Parameter(torch.normal(mean=0.,
+                                                 std=0.05,
+                                                 size=(layer.weight.shape)
+                                                 ))
+        self.g = torch.nn.Parameter(torch.normal(mean=0.0,
+                                                 std=0.05,
+                                                 size=(1,)
+                                                 ))
+
+    def forward(self, x):
+        weight = (self.g / torch.norm(self.v.T, dim=-1)) * self.v
+        outputs = torch.matmul(x, weight.T) + self.layer.bias
+        return outputs
+
+
+norms = {"BatchNorm (me)": BatchNorm,
+         "BatchNorm (torch)": nn.BatchNorm2d,
+         "LayerNorm (me)": LayerNorm,
+         "LayerNorm (torch)": nn.LayerNorm,
+         "WeightNorm (me)": WeightNorm,
+         "WeightNorm (torch)": WN,
+         "No Norm": None}
+
+
+num_classes = 10
+hidden_size = 50
+n_epochs = 5
+
+
+def run_experiment(norms, output):
+    results = pd.DataFrame(columns=["Test Acc", "Train Loss", "Norm"],
+                           dtype=object)
+
+    for key in norms:
+        train_losses = np.zeros((n_epochs))
+        torch.manual_seed(99111100121)
+        model = CNN(hidden_size, num_classes, optim.Adam, norms[key])
+        print(f"Training with norm: {key}")
+        for epoch in range(n_epochs):
+            train_loss = model.train_step(trainloader)
+            print(f"Epoch {epoch}: {train_loss}")
+            train_losses[epoch] = train_loss
+        test_acc = model.test_model(testloader)
+        print(f"Test Acc {test_acc}")
+
+        new_r = pd.DataFrame({"Test Acc": test_acc,
+                              "Train Loss": [train_losses],
+                              "Norm": key})
+
+        results = pd.concat([results, new_r], ignore_index=True)
+
+    return results
+
+
+results = run_experiment(norms, None)
+
+
+colors = ["red", "blue", "green", "black", "yellow", "orange", "pink"]
+plt.bar(results["Norm"].to_list(), results["Test Acc"].to_numpy(),
+        color=colors, log=True)
+plt.ylabel("Log of Test Accuracy")
+plt.xlabel("Type of Normalization")
+
+plt.show()
+
+for loss, norm, color in zip(results["Train Loss"], results["Norm"], colors):
+    plt.plot(loss, label=norm, color=color, linewidth=2.5)
+
+plt.xticks([0, 1, 2, 3, 4])
+plt.xlabel("Training Epoch")
+plt.ylabel("Training Loss")
+plt.legend()
+plt.show()
+
+plt.bar("BatchNorm",
+        -np.diff(results[results["Norm"].str.contains("BatchNorm")]
+                 ["Test Acc"].to_numpy()), color="red")
+plt.bar("LayerNorm",
+        -np.diff(results[results["Norm"].str.contains("LayerNorm")]
+                 ["Test Acc"].to_numpy()), color="green")
+plt.bar("WeightNorm",
+        -np.diff(results[results["Norm"].str.contains("WeightNorm")]
+                 ["Test Acc"].to_numpy()), color="blue")
+plt.show()
